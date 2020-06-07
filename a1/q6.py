37ea01d03957ca4368f58660fc4fca998fb02323
Zahin Mohammad <zahin.mohammad@gmail.com>
2020-06-07 19:43:53 -0400

    finish

--- a1/q6.py
+++ a1/q6.py
@@ -1,5 +1,3 @@
-import sys
-
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -38,13 +36,76 @@ maze = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ]
 def heuristic(currState, goalState):
+    # Euclidean distance
     return (currState[0]-goalState[0])**2 + (currState[1] -goalState[1])**2
 
-def aStar(state, goalState):
-    pass
+def aStar(initState, goalState):
+    path, nodesVisited, nodesExpanded, goalReached = [], 1, 1, False
+    # maps state to its parent
+    bfsMap = {}
 
+    # Stack stores elements in the form of (heuristic + cost, row, col)
+    # Stored in Descending order
+    stack = [(heuristic(initState, goalState), initState[0], initState[1])]
+
+    while len(stack):
+        # print(stack)
+        state = stack.pop()
+        nodesExpanded +=1
+        if (state[1], state[2]) == goalState:
+            goalReached = True
+            break
+        row = state[1]
+        col = state[2]
+        
+        # Move Up
+        if (row-1, col) not in bfsMap and row-1 >= 0 and not maze[row-1][col]:
+            nodesVisited +=1
+            childCost = heuristic((row-1, col), goalState)
+            stack.append((childCost, row-1, col))
+            bfsMap[(row-1, col)] = (row, col)
+        
+        # Move Down
+        if (row+1, col) not in bfsMap and row+1 < len(maze) and not maze[row+1][col]:
+            nodesVisited +=1
+            childCost = heuristic((row+1, col), goalState)
+            stack.append((childCost, row+1, col))
+            bfsMap[(row+1, col)] = (row, col)
+        
+        # Move Left 
+        if (row, col-1) not in bfsMap and col-1 >= 0 and not maze[row][col-1]:
+            nodesVisited +=1
+            childCost = heuristic((row, col-1), goalState)
+            stack.append((childCost, row, col-1))
+            bfsMap[(row, col-1)] = (row, col)
+
+        # Move Right
+        if (row, col+1) not in bfsMap and col+1 < len(maze[0]) and not maze[row][col+1]:
+            nodesVisited +=1
+            childCost = heuristic((row, col+1), goalState)
+            stack.append((childCost, row, col+1))
+            bfsMap[(row, col+1)] = (row, col)
+
+        stack.sort(reverse=True)
+    
+    if not goalReached:
+        return path, nodesVisited, goalReached
+    
+    path.append(goalState)
+    cost = 1
+    traverseState = bfsMap[goalState]
+    while True:
+        cost +=1
+        path.append(traverseState)
+        if traverseState == initState:
+            break
+        else:
+            traverseState = bfsMap[traverseState]
+
+    return list(reversed(path)), nodesVisited, nodesExpanded, goalReached
+    
 def bfsOrDfs(initState, goalState, isBfs):
-    path, nodesVisited, goalReached = [], 0, False
+    path, nodesVisited, nodesExpanded, goalReached = [], 1, 1, False
     # maps state to its parent
     bfsMap = {}
 
@@ -52,7 +113,7 @@ def bfsOrDfs(initState, goalState, isBfs):
 
     while len(stack):
         state = stack.pop(0) if isBfs else stack.pop()
-        nodesVisited +=1
+        nodesExpanded +=1
         if state == goalState:
             goalReached = True
             break
@@ -62,21 +123,25 @@ def bfsOrDfs(initState, goalState, isBfs):
         
         # Move Up
         if (row-1, col) not in bfsMap and row-1 >= 0 and not maze[row-1][col]:
+            nodesVisited+=1
             stack.append((row-1, col))
             bfsMap[(row-1, col)] = state
         
         # Move Down
         if (row+1, col) not in bfsMap and row+1 < len(maze) and not maze[row+1][col]:
+            nodesVisited+=1
             stack.append((row+1, col))
             bfsMap[(row+1, col)] = state
         
         # Move Left 
         if (row, col-1) not in bfsMap and col-1 >= 0 and not maze[row][col-1]:
+            nodesVisited+=1
             stack.append((row, col-1))
             bfsMap[(row, col-1)] = state
 
         # Move Right
         if (row, col+1) not in bfsMap and col+1 < len(maze[0]) and not maze[row][col+1]:
+            nodesVisited+=1
             stack.append((row, col+1))
             bfsMap[(row, col+1)] = state
     
@@ -94,8 +159,7 @@ def bfsOrDfs(initState, goalState, isBfs):
         else:
             traverseState = bfsMap[traverseState]
 
-    return list(reversed(path)), nodesVisited, goalReached
-
+    return list(reversed(path)), nodesVisited, nodesExpanded, goalReached
 
 def convertToTopLeft(state):
     # 1,0 -> 24, 1
@@ -126,38 +190,39 @@ def printPath(path):
         pathString += str(convertToCartesian(coord))
     print(pathString)
 
-
 def test(init, goal):
-    dfsPath, visited, goalReached = bfsOrDfs(init, goal, False)
+    dfsPath, visited, expanded, goalReached = bfsOrDfs(init, goal, False)
     print(f"DFS: {convertToCartesian(init)} -> {convertToCartesian(goal)}")
     print(f"Cost: {len(dfsPath)}")
     print(f"Visited: {visited}")
+    print(f"Expanded: {expanded}")
     print("")
     printColoredMaze(dfsPath, init, goal)
     print("")
     printPath(dfsPath)
     print("")
 
-    bfsPath, visited, goalReached = bfsOrDfs(init, goal, True)
+    bfsPath, visited, expanded, goalReached = bfsOrDfs(init, goal, True)
     print(f"BFS: {convertToCartesian(init)} -> {convertToCartesian(goal)}")
     print(f"Cost: {len(bfsPath)}")
     print(f"Visited: {visited}")
+    print(f"Expanded: {expanded}")
     print("")
     printColoredMaze(bfsPath, init, goal)
     print("")
     printPath(bfsPath)
     print("")
 
-    aStarPath, visited, goalReached = aStar(init, goal)
-    print(f"BFS: {convertToCartesian(init)} -> {convertToCartesian(goal)}")
+    aStarPath, visited, expanded, goalReached = aStar(init, goal)
+    print(f"A*: {convertToCartesian(init)} -> {convertToCartesian(goal)}")
     print(f"Cost: {len(aStarPath)}")
     print(f"Visited: {visited}")
+    print(f"Expanded: {expanded}")
     print("")
     printColoredMaze(aStarPath, init, goal)
     print("")
     printPath(aStarPath)
     print("")
-
 
 
 init = convertToTopLeft((0,0))
@@ -171,145 +236,3 @@ test(init, goal)
 init = convertToTopLeft((2,11))
 goal = convertToTopLeft((23,19))
 test(init, goal)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def dfs(initState, goalState):
-#     path, nodesVisited, goalReached = [], 0, False
-#     # maps state to its parent
-#     bfsMap = {}
-
-#     stack = [initState]
-
-#     while len(stack):
-#         state = stack.pop()
-#         nodesVisited +=1
-#         if state == goalState:
-#             goalReached = True
-#             break
-#         row = state[0]
-#         col = state[1]
-#         stateChildren = []
-        
-#         # Move Up
-#         if (row-1, col) not in bfsMap and row-1 >= 0 and not maze[row-1][col]:
-#             stack.append((row-1, col))
-#             bfsMap[(row-1, col)] = state
-        
-#         # Move Down
-#         if (row+1, col) not in bfsMap and row+1 < len(maze) and not maze[row+1][col]:
-#             stack.append((row+1, col))
-#             bfsMap[(row+1, col)] = state
-        
-#         # Move Left 
-#         if (row, col-1) not in bfsMap and col-1 >= 0 and not maze[row][col-1]:
-#             stack.append((row, col-1))
-#             bfsMap[(row, col-1)] = state
-
-#         # Move Right
-#         if (row, col+1) not in bfsMap and col+1 < len(maze[0]) and not maze[row][col+1]:
-#             stack.append((row, col+1))
-#             bfsMap[(row, col+1)] = state
-    
-#     if not goalReached:
-#         return path, nodesVisited, goalReached
-    
-#     path.append(goalState)
-#     cost = 1
-#     traverseState = bfsMap[goalState]
-#     while True:
-#         cost +=1
-#         path.append(traverseState)
-#         if traverseState == initState:
-#             break
-#         else:
-#             traverseState = bfsMap[traverseState]
-
-#     return list(reversed(path)), nodesVisited, goalReached
-
-
-# dfsMap = set()
-# def dfs(state, goalState): # Return Path, totalCost, goalReached 
-#     # Goal Step
-#     dfsMap.add(state)
-#     if state == goalState:
-#         return [state], len(dfsMap), True
-
-#     # Not at Goal, go one level deeper
-#     row = state[0]
-#     col = state[1]
-
-#     # Add nodes to LIFO stack in order Up, Down, Left, Right
-#     # Therefore process in order of Right, Left, Down, Up
-
-#     # Move Right
-#     if (row, col+1) not in dfsMap and col +1 < len(maze[0]) and not maze[row][col+1]:
-#         path, cost, goalReached = dfs((row, col+1), goalState)
-#         if goalReached:
-#             newPath = [state]
-#             newPath.extend(path)
-#             return newPath, len(dfsMap), True
-#     # Move Left
-#     if (row, col-1) not in dfsMap and col -1 >=0 and not maze[row][col-1]:
-#         path, cost, goalReached = dfs((row, col-1), goalState)
-#         if goalReached:
-#             newPath = [state]
-#             newPath.extend(path)
-#             return newPath, len(dfsMap), True
-#     # Move Down
-#     if (row+1, col) not in dfsMap and row + 1 < len(maze) and not maze[row+1][col]:
-#         path, cost, goalReached = dfs((row+1, col), goalState)
-#         if goalReached:
-#             newPath = [state]
-#             newPath.extend(path)
-#             return newPath, len(dfsMap), True
-#     # Move Up
-#     if (row-1, col) not in dfsMap and row -1 >= 0 and not maze[row-1][col]:
-#         path, cost, goalReached = dfs((row-1, col), goalState)
-#         if goalReached:
-#             newPath = [state]
-#             newPath.extend(path)
-#             return newPath, len(dfsMap), True
-#     return [], len(dfsMap), False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# initRow = int(input("Enter Initial Row : "))
-# initCol = int(input("Enter Initial Col : "))
-
-# if initRow >= len(maze) or initCol >= len(maze[0]) or maze[initRow][initCol]:
-#     print("Invalid initial state")
-#     sys.exit()
