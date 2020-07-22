11ff2e8dcb837a71c4ed7f83827d64e6a2dc3b1f
Zahin Mohammad <zahin.mohammad@gmail.com>
2020-07-22 09:59:44 -0400

    minor script change

--- assignments/a3/q1/parallel.sh
+++ assignments/a3/q1/parallel.sh
@@ -2,11 +2,8 @@ counter=0
 while [ $counter -le 4 ]
 do
    
-    # your_command_string=""
-    # output=$(eval "$your_command_string")
     taskset -c $counter python ./genetic_algorithm.py $counter &
     echo $counter
-    echo "$output"
     counter=$(( $counter + 1 ))
 
 done
