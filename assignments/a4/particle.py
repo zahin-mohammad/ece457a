777cae2b57161beda963ac689d2c61d34f82b2da
Zahin Mohammad <zahin.dev@gmail.com>
2020-07-31 18:54:38 -0400

    finish particle?

--- assignments/a4/particle.py
+++ assignments/a4/particle.py
@@ -8,12 +8,14 @@ class Particle(object):
                  y_range,
                  position,
                  velocity):
+
         self.position = position
         if not self.position:
             self.position = np.array([
                 np.random.uniform(x_range[0], x_range[1]),
                 np.random.uniform(y_range[0], y_range[1]),
             ])
+
         self.velocity = velocity
         self.p_best = self.p_best
 
@@ -21,15 +23,13 @@ class Particle(object):
             global_best,
             velocity_update_fn,
             fitness_fn):
+
         # Update velocity
         self.velocity = velocity_update_fn(self, global_best)
 
         # Update position
         self.position = self.position * self.velocity
 
-        # update velocity, update position, update personal_best
-
-        # particle.fly(
-        #     particle_neighborhood(particles),
-        #     velocity_update,
-        #     fitness_function)
+        # update personal_best
+        self.p_best = max([self.p_best, self.position],
+                          key=lambda pos: fitness_fn(pos[0], pos[1]))
